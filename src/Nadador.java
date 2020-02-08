8571d2db137913c10bb039d586a6b0d15a6b2b7f
niluiz <nicoleduarteluna@hotmail.com>
2020-02-07 22:25:50 -0300

    Aula de Java 1

---
+++ src/Nadador.java
@@ -0,0 +1,20 @@
+
+public class Nadador {
+	public static void main(String[] args) {
+		int idade = 16;
+
+		if (idade >= 5 && idade <= 7) {
+			System.out.println("INFATIL");
+		} else if (idade >= 8 && idade <= 10) {
+			System.out.println("JUVENIL");
+		} else if (idade >= 11 && idade <= 15) {
+			System.out.println("ADOLESCENTE");
+		} else if (idade >= 16 && idade <= 30) {
+			System.out.println("ADULTO");
+		} else if (idade >30){
+			System.out.println("SÊNIOR");
+		} else {
+			System.out.println("NENHUM");
+		}
+	}
+}
---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+	String nome ="Nicole";
+	String sobrenome= "Duarte";
+	
+	System.out.println("Olá " + nome + " " + sobrenome + "!");
+	System.out.println(String.format("Olá, %s %s", nome, sobrenome));
+	}
+}
+
+
+
